9487c891c8ca62533ecf8d4ba07cb3ca39f39547
Parin Kittipongdaja <parin.kiti@gmail.com>
2021-01-12 13:20:24 +0700

    Modify Window Level Function

--- labelme/widgets/brightness_contrast_dialog.py
+++ labelme/widgets/brightness_contrast_dialog.py
@@ -13,14 +13,14 @@ class BrightnessContrastDialog(QtWidgets.QDialog):
     def __init__(self, img, callback, parent=None):
         super(BrightnessContrastDialog, self).__init__(parent)
         self.setModal(True)
-        self.setWindowTitle("Brightness/Contrast")
+        self.setWindowTitle("Window_Level/Window_Width")
 
-        self.slider_window = self._create_slider()
-        self.slider_window_size = self._create_slider()
+        self.slider_window = self._create_slider_WL()
+        self.slider_window_size = self._create_slider_WW()
 
         formLayout = QtWidgets.QFormLayout()
-        formLayout.addRow(self.tr("Window"), self.slider_window)
-        formLayout.addRow(self.tr("Window size"), self.slider_window_size)
+        formLayout.addRow(self.tr("Window Level"), self.slider_window)
+        formLayout.addRow(self.tr("Window Width"), self.slider_window_size)
         self.setLayout(formLayout)
 
         assert isinstance(img, PIL.Image.Image)
@@ -28,16 +28,16 @@ class BrightnessContrastDialog(QtWidgets.QDialog):
         self.callback = callback
 
     def onNewValue(self, value):
-        window = self.slider_window.value() / 50.0
-        window_size = self.slider_window_size.value() / 50.0
+        window = self.slider_window.value()
+        window_size = self.slider_window_size.value()
         print('window:', window)
         print('window size:', window_size)
 
         img = self.img
         img = np.asarray(img)
 
-        low = window * 1000
-        high = low + window_size * 1000
+        low = window - window_size/2
+        high = window + window_size/2
         img = change_window(img, low, high)
         # img = self.img
         # img = PIL.ImageEnhance.Brightness(img).enhance(brightness)
@@ -49,17 +49,25 @@ class BrightnessContrastDialog(QtWidgets.QDialog):
         qimage = QtGui.QImage.fromData(img_data)
         self.callback(qimage)
 
-    def _create_slider(self):
+    def _create_slider_WL(self):
         slider = QtWidgets.QSlider(Qt.Horizontal)
-        slider.setRange(0, 150)
-        slider.setValue(50)
+        slider.setRange(0, 4000)
+        slider.setValue(2000)
+        slider.setTickInterval(100)
+        slider.setSingleStep(100)
+        slider.valueChanged.connect(self.onNewValue)
+        return slider
+
+    def _create_slider_WW(self):
+        slider = QtWidgets.QSlider(Qt.Horizontal)
+        slider.setRange(0, 2000)
+        slider.setValue(1000)
+        slider.setTickInterval(100)
+        slider.setSingleStep(100)
         slider.valueChanged.connect(self.onNewValue)
         return slider
 
 
 def change_window(img, low, high):
-    MAX = 2**16 - 1
-    low = 1000
-    high = 3000
-    img = (img.clip(min=low, max=high) - low) / (high - low) * MAX
-    return img.astype(np.uint16)
+    img = (img.clip(min=low, max=high) - low) / (high - low) * 255
+    return img.astype(np.uint8)
